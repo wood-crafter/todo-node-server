9448bd8b27d01ceef4400a00e9c698b4f75524e4
hungpv <phanhung2702@gmail.com>
2023-01-11 17:34:09 +0700

    [Draft] login api

--- index.js
+++ index.js
@@ -4,8 +4,35 @@ const app = express()
 
 app.use(cors())
 
+const USERS = [
+  { username: 'vnanne', password: '1' },
+  { username: 'gogi', password: '1' },
+  { username: 'aiinauu', password: '1' }
+]
+
 app.get('/', function (req, res) {
   res.send("App listening");
+})
+
+app.post('/login', function (req, res) {
+  const username = req.username
+  const password = req.password
+
+  USERS.forEach(element => {
+    if (element.username === username && element.password === password) {
+      res.send({
+        code: 200,
+        data: {
+          username,
+          password
+        }
+      })
+    }
+  })
+
+  res.send({
+    code: 201
+  })
 })
 
 app.listen(9083, () => {
